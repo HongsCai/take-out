9bd94b7ab1013e8756a2fb50598661b16fd90bcf
Hongs_Cai <Hongs_Cai@outlook.com>
2025-12-04 14:23:37 +0800

    update

--- sky-take-out/sky-server/src/main/java/com/hongs/skyserver/controller/admin/EmployeeController.java
+++ sky-take-out/sky-server/src/main/java/com/hongs/skyserver/controller/admin/EmployeeController.java
@@ -1,9 +1,11 @@
 package com.hongs.skyserver.controller.admin;
 
+import com.hongs.skycommon.constant.JwtClaimsConstant;
 import com.hongs.skycommon.pojo.dto.EmployeeLoginDTO;
 import com.hongs.skycommon.pojo.entity.Employee;
 import com.hongs.skycommon.properties.JwtProperties;
 import com.hongs.skycommon.pojo.vo.EmployeeLoginVO;
+import com.hongs.skycommon.utils.JwtUtil;
 import com.hongs.skyserver.common.BaseResponse;
 import com.hongs.skyserver.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
@@ -28,6 +30,12 @@ public class EmployeeController {
     @Autowired
     private JwtProperties jwtProperties;
 
+    /**
+     * 登录
+     *
+     * @param employeeLoginDTO
+     * @return
+     */
     @PostMapping("/login")
     public BaseResponse<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoginDTO) {
         log.info("员工登录: {}", employeeLoginDTO);
@@ -35,9 +43,26 @@ public class EmployeeController {
 
         // 登录成果生成Jwt令牌
         Map<String, Object> claims = new HashMap<>();
+        claims.put(JwtClaimsConstant.EMP_ID, employee.getId());
+        String token = JwtUtil.createJWT(jwtProperties.getAdminSecretKey(), jwtProperties.getAdminTtl(), claims);
 
+        EmployeeLoginVO employeeLoginVO = EmployeeLoginVO.builder()
+                .id(employee.getId())
+                .userName(employee.getUsername())
+                .name(employee.getName())
+                .token(token)
+                .build();
 
+        return BaseResponse.success(employeeLoginVO);
     }
 
-
+    /**
+     * 退出
+     *
+     * @return
+     */
+    @PostMapping("/logout")
+    public BaseResponse<String> logout() {
+        return BaseResponse.success();
+    }
 }
